91ccad255ff4297188e84e3e403af65371acd1d0
Julio Mata <julioematasv@gmail.com>
2022-06-09 08:27:01 +0200

    Creado enlace a pagina de estilos en index.php

--- U_2_IntroPHP/index.php
+++ U_2_IntroPHP/index.php
@@ -4,10 +4,24 @@
   $nombre = "Julio";
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Primer Script PHP</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-<div class="content">
+  <div class="content">
 
-  <h1><?php echo $saludo . $nombre; ?></h1>
+    <h1><?php echo $saludo . $nombre; ?></h1>
 
-</div>
+  </div>
+
+</body>
+</html>
+
 
